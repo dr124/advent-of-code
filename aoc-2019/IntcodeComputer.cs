80888027ac2f89c13e759b7fb7136d7f4f67af1d
Dominik Ruman <dr124@o2.pl>
2019-12-07 10:29:10 +0100

    Update IntcodeComputer.cs

--- aoc-2019/IntcodeComputer.cs
+++ aoc-2019/IntcodeComputer.cs
@@ -8,38 +8,56 @@ namespace advent
 {
     public class IntcodeComputer
     {
-        private List<int> ROM { get; } // read only memory
-        public List<int> Memory { get; set; } // program memory
+        public int[] Memory { get; set; } // program memory
 
-        public IntcodeComputer(int[] instructions)
+        // for diag codes
+        public List<int> Input;
+        public List<int> Output;
+        private int InputPointer;
+        private int InputValue => Input[InputPointer++];
+        private int OutputValue
         {
-            ROM = instructions.ToList();
-            Memory = ROM.ToList();
+            set => Output.Add(value);
         }
 
-        public void Process()
-        {   
-            for (var i = 0; i < Memory.Count;)
-                i = OpCode(i);
+        private readonly int[] ROM; // read only memory
+        private Instruction Instr;  // current instruction
+        private int Pointer;        // instruction pointer
+        private bool Stop;
+
+        public IntcodeComputer(int[] Instructions)
+        {
+            ROM = Instructions.ToArray();
+            ResetMemory();
+        }
+
+        public void Compute()
+        {
+            while (!Stop)
+            {
+                Pointer = OpCode(Pointer);
+            }
         }
 
         public void ResetMemory()
         {
-            Memory = ROM.ToList();
+            Memory = ROM.ToArray();
+            Stop = false;
+            Pointer = 0;
+            Output = new List<int>();
+            Input = new List<int>();
+            InputPointer = 0;
         }
-
-        public int DiagCode { get; set; }
 
         public int MathCodes(int i)
         {
-            var instr = new Instruction(Memory, i);
-            switch (instr.Instr)
+            switch (Instr.Instr)
             {
                 case 1:
-                    Memory[instr.Param3Value] = instr.Param2Value + instr.Param1Value;
+                    Memory[Instr.Param3Value] = Instr.Param2Value + Instr.Param1Value;
                     break;
                 case 2:
-                    Memory[instr.Param3Value] = instr.Param2Value * instr.Param1Value;
+                    Memory[Instr.Param3Value] = Instr.Param2Value * Instr.Param1Value;
                     break;
             }
 
@@ -48,17 +66,13 @@ namespace advent
 
         public int DiagCodes(int i)
         {
-            var instr = new Instruction(Memory, i);
-            switch (instr.Instr)
+            switch (Instr.Instr)
             {
                 case 3: // input a value
-                    Console.WriteLine("INPUT CODE");
-                    Memory[Memory[i + 1]] = DiagCode;
+                    Memory[Memory[i + 1]] = InputValue; 
                     break;
                 case 4: // output a value
-                    var xd = Memory[Memory[i + 1]];
-                    Console.WriteLine($"{xd}");
-                    Debug.WriteLine($"{xd}");
+                    OutputValue = Memory[Memory[i + 1]];
                     break;
             }
 
@@ -67,16 +81,15 @@ namespace advent
 
         public int JumpCodes(int i)
         {
-            var instr = new Instruction(Memory, i);
-            switch (instr.Instr)
+            switch (Instr.Instr)
             {
                 case 5: // jump if true 
-                    if (instr.Param1Value != 0)
-                        return instr.Param2Value;
+                    if (Instr.Param1Value != 0)
+                        return Instr.Param2Value;
                     break;
                 case 6: // jump is zero
-                    if (instr.Param1Value == 0)
-                        return instr.Param2Value;
+                    if (Instr.Param1Value == 0)
+                        return Instr.Param2Value;
                     break;
             }
 
@@ -85,14 +98,13 @@ namespace advent
 
         public int IfCodes(int i)
         {
-            var instr = new Instruction(Memory, i);
-            switch (instr.Instr)
+            switch (Instr.Instr)
             {
                 case 7: // less than
-                    Memory[instr.Param3Value] = instr.Param1Value < instr.Param2Value ? 1 : 0;
+                    Memory[Instr.Param3Value] = Instr.Param1Value < Instr.Param2Value ? 1 : 0;
                     break;
                 case 8: // equals
-                    Memory[instr.Param3Value] = instr.Param1Value == instr.Param2Value ? 1 : 0;
+                    Memory[Instr.Param3Value] = Instr.Param1Value == Instr.Param2Value ? 1 : 0;
                     break;
             }
 
@@ -101,8 +113,15 @@ namespace advent
 
         public int OpCode(int i)
         {
-            var instr = new Instruction(Memory, i);
-            switch (instr.Instr)
+            if (Pointer >= Memory.Length)
+            {
+                Stop = true;
+                return 0;
+            }
+
+            Instr = new Instruction(Memory.ToList(), Pointer);
+
+            switch (Instr.Instr)
             {
                 case 1:
                 case 2:
@@ -117,7 +136,7 @@ namespace advent
                 case 8:
                     return IfCodes(i);
                 case 99:
-                    return Memory.Count; // program exit
+                    return Memory.Length; // program exit
                 default:
                     throw new Exception("witam, error, pozdrawiam");
             }
@@ -125,14 +144,15 @@ namespace advent
 
         public class Instruction
         {
+            private string raw;
             public int Instr;
             public int Param1Value, Param2Value, Param3Value;
-            public int? Param1Mode, Param2Mode, Param3Mode;
+            public int Param1Mode, Param2Mode, Param3Mode;
 
             public Instruction(List<int> list, int i)
             {
                 var str = list[i].ToString();
-
+                raw = str;
                 // one digit instr
                 Instr = list[i];
 
