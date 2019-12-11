d1c38b9fe414a5623777e9f4c83bb7c29c81d2f8
Dominik Ruman <dr124@o2.pl>
2019-12-11 17:26:34 +0100

    day 10 refactor

--- advent2019/Week2/Day10.cs
+++ advent2019/Week2/Day10.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using MoreLinq;
 
 namespace advent2019.Week2
 {
@@ -22,15 +23,22 @@ namespace advent2019.Week2
 
         public static (int, int) Process()
         {
-            foreach (var p in GetAllPoints())
-                MapPoint(p) = FindVisibleAsteroids(p);
+            var bestPoint = GetAllPoints()
+                .Select(x => new
+                {
+                    point = x,
+                    visibleAsteroids = GetAllPoints()
+                        .Except(new[] {x})
+                        .Select(p => (x - p).AngleInDegrees)
+                        .GroupBy(y => y)
+                        .Count()
+                }).MaxBy(x => x.visibleAsteroids)
+                .First();
 
-            var taskA = map.Max(x => x.Max());
+            var taskA = bestPoint.visibleAsteroids;
+            var laser = bestPoint.point;
 
-            var laser = GetAllPoints().First(p => MapPoint(p) == taskA);
-            Console.WriteLine($"Best point: ({laser.X},{laser.Y})");
-
-            var groupedPoints = GetAllPoints()
+            var destroyedByLaser = GetAllPoints()
                 .Except(new[] {laser})
                 .Select(p => new
                 {
@@ -45,7 +53,7 @@ namespace advent2019.Week2
                     .ToList())
                 .ToList();
 
-            var taskB = groupedPoints[199][0];
+            var taskB = destroyedByLaser[199][0];
 
             return (taskA, taskB);
         }
@@ -56,74 +64,6 @@ namespace advent2019.Week2
             for (var x = 0; x < map.Length; x++)
                 if (MapPoint((x, y)) != 0)
                     yield return new Point(x, y);
-        }
-
-        public static int FindVisibleAsteroids(Point a)
-        {
-            return GetAllPoints()
-                .Where(p => p != a)
-                .Count(p => !GetCollidingAsteroids(a, p).Any());
-        }
-
-        public static IEnumerable<Point> GetCollidingAsteroids(Point A, Point B)
-        {
-            var d = B - A;
-            return (d.X, d.Y) switch
-            {
-                (0, 0) => Enumerable.Empty<Point>(),
-                (0, _) => GetCollidingY(A, B),
-                (_, 0) => RayCollidesX(A, B),
-                _ => GetCollidingDiagonal(A, B)
-            };
-        }
-
-        public static IEnumerable<Point> RayCollidesX(Point A, Point B)
-        {
-            var dx = (B - A).X;
-            var stepX = dx / Math.Abs(dx);
-
-            for (var i = 1; i < Math.Abs(dx); i++)
-            {
-                var p = A + (i * stepX, 0);
-                if (MapPoint(p) != 0)
-                    yield return p;
-            }
-        }
-
-        public static IEnumerable<Point> GetCollidingY(Point A, Point B)
-        {
-            var d = B - A;
-            var stepY = d.Y / Math.Abs(d.Y);
-
-            for (var i = 1; i < Math.Abs(d.Y); i++)
-            {
-                var p = A + (0, i * stepY);
-                if (MapPoint(p) != 0)
-                    yield return p;
-            }
-        }
-
-        public static IEnumerable<Point> GetCollidingDiagonal(Point A, Point B)
-        {
-            var d = B - A;
-            var stepY = d.X / Math.Abs(d.X);
-            var dydx = (double) d.Y / d.X;
-
-            var a = dydx;
-            var b = A.Y - dydx * A.X;
-
-            for (var i = 1; i <= Math.Abs(d.X); i++)
-            {
-                var x = A.X + stepY * i;
-                var y = a * x + b;
-                var ry = (int) Math.Round(y);
-                var p = new Point(x, ry);
-
-                if (Math.Abs(y - Math.Round(y)) <= 1e-5
-                    && p != A && p != B
-                    && MapPoint(p) != 0)
-                    yield return p;
-            }
         }
 
         public static ref int MapPoint(this Point p) => ref map[p.Y][p.X];
