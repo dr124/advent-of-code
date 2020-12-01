f8a99425e2a40229891702e673b6cd79e7fa7a28
Dominik Ruman <dr124@o2.pl>
2020-12-01 11:15:05 +0100

    day1

---
+++ Advent._2020/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+using Advent._2020.Week1;
+
+namespace Advent._2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Stopwatch();
+            s.Start();
+
+            Day1.Execute();
+
+            s.Stop();
+            Console.WriteLine($"\n\nTime Elapsed: {s.ElapsedMilliseconds}ms");
+        }
+    }
+}
---
+++ Advent._2020/Week1/Day1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Advent._2020.Week1
+{
+    public class Day1
+    {
+        public static void Execute()
+        {
+            var input = File.ReadAllLines("Week1/input1.txt")
+                .Select(int.Parse)
+                .ToArray();
+
+            var taskA = TaskA(input);
+            var taskB = TaskB(input);
+            Console.WriteLine($"A: {taskA}, B: {taskB}");
+        }
+
+        public static int TaskA(int[] input)
+        {
+            foreach (var i in input)
+            foreach (var j in input)
+                if (i + j == 2020)
+                    return i * j;
+            return -1;
+        }
+
+        public static int TaskB(int[] input)
+        {
+            foreach (var i in input)
+            foreach (var j in input)
+            foreach (var k in input)
+                if (i + j + k == 2020)
+                    return i * j * k;
+            return -1;
+        }
+    }
+}
