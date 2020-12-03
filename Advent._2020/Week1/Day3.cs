d5ec977e587a4bc4d8e044d74e6d1c9a33d6f817
Dominik Ruman <dr124@o2.pl>
2020-12-03 11:30:39 +0100

    day3 functional

--- Advent._2020/Week1/Day3.cs
+++ Advent._2020/Week1/Day3.cs
@@ -1,49 +1,33 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using Advent.Core;
 
 namespace Advent._2020.Week1
 {
-    public class Day3 : Day<bool[][], int>
+    public class Day3 : Day<string[], int>
     {
-        protected override bool[][] ReadData()
+        private readonly (int x, int y)[] Slopes =
         {
-            return File.ReadAllLines("Week1/input3.txt")
-                .Select(y => y.Select(x => x == '#').ToArray())
-                .ToArray();
-        }
+            (x: 1, y: 1),
+            (x: 3, y: 1),
+            (x: 5, y: 1),
+            (x: 7, y: 1),
+            (x: 1, y: 2)
+        };
 
-        protected override int TaskA()
+        protected override string[] ReadData() => File.ReadAllLines("Week1/input3.txt");
+
+        protected override int TaskA() => TreesInSlope((3, 1));
+        protected override int TaskB() => Slopes.Select(TreesInSlope).Product();
+
+        private int TreesInSlope((int x, int y) slope) => EnumerateSlope(slope).Count(IsDżewo);
+        private bool IsDżewo((int x, int y) pos) => Input[pos.y % Input.Length][pos.x % Input[0].Length] == '#';
+
+        private IEnumerable<(int x, int y)> EnumerateSlope((int x, int y) slope)
         {
-            return TreesInSlope((3, 1));
-        }
-
-        protected override int TaskB()
-        {
-            var slopes = new[]
-            {
-                (x: 1, y: 1),
-                (x: 3, y: 1),
-                (x: 5, y: 1),
-                (x: 7, y: 1),
-                (x: 1, y: 2)
-            };
-
-            return slopes.Select(TreesInSlope).Product();
-        }
-
-        private int TreesInSlope((int x, int y) slope)
-        {
-            var trees = 0;
             for (int y = 0, x = 0; y < Input.Length; y += slope.y, x += slope.x)
-                if (IsTree(y, x))
-                    trees++;
-            return trees;
-        }
-
-        private bool IsTree(int y, int x)
-        {
-            return Input[y % Input.Length][x % Input[0].Length];
+                yield return (x, y);
         }
     }
 }
