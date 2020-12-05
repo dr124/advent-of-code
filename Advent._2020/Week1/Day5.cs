87fc85f12f2bbe2d384fd076bbe227f07a9bb6eb
Dominik Ruman <dr124@o2.pl>
2020-12-05 13:52:43 +0100

    day 5 with int[]

--- Advent._2020/Week1/Day5.cs
+++ Advent._2020/Week1/Day5.cs
@@ -1,60 +1,49 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using System.Linq;
 using Advent.Core;
 
 namespace Advent._2020.Week1
 {
-    public class Day5 : Day<Day5.Seat[], int>
+    public class Day5 : Day<int[], int>
     {
-        protected override Seat[] ReadData()
+        protected override int[] ReadData()
         {
             return File.ReadAllLines("Week1/input5.txt")
-                .Select(Seat.Create)
+                .Select(str => 8 * BinarySearch(str[..7], 'F', 'B')
+                               + BinarySearch(str[^3..], 'L', 'R'))
                 .ToArray();
         }
 
         protected override int TaskA()
         {
-            return Input.Max(x => x.SeatId);
+            return Input.Max();
         }
 
         protected override int TaskB()
         {
-            var seats = Input.OrderBy(x => x.SeatId).ToList();
+            var seats = Input.OrderBy(x => x).ToList();
             for (var i = 1; i < seats.Count; i++)
-                if (seats[i].SeatId - seats[i - 1].SeatId != 1)
-                    return seats[i - 1].SeatId + 1;
+                if (seats[i] - seats[i - 1] != 1)
+                    return seats[i - 1] + 1;
 
             return -1;
         }
 
-        public record Seat(int Row, int Col)
+        // ========================================
+
+        private static int BinarySearch(string str, char l, char r)
         {
-            public int SeatId => Row * 8 + Col;
-
-            public static Seat Create(string str)
+            int from = 0, to = (1 << str.Length) - 1;
+            foreach (var t in str)
             {
-                return new (
-                    BinarySearch(str[..7], 'F', 'B'),
-                    BinarySearch(str[^3..], 'L', 'R'));
-
-                static int BinarySearch(string str, char l, char r)
-                {
-                    var from = 0;
-                    var to = (1 << str.Length) - 1;
-                    foreach (var t in str)
-                    {
-                        var mid = (to + from) / 2;
-                        if (t == l)
-                            to = mid;
-                        else if (t == r)
-                            from = mid + 1;
-                    }
-
-                    return to;
-                }
+                var mid = (to + from) / 2;
+                if (t == l) 
+                    to = mid;
+                else if (t == r) 
+                    from = mid + 1;
             }
+
+            return to;
         }
     }
 }
