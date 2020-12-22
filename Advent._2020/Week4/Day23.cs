559a2c4c68ac44eb92344246da812bc7871e9ae4
Dominik Ruman <dr124@o2.pl>
2020-12-22 01:46:59 +0100

    init week 4

---
+++ Advent._2020/Week4/Day23.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent._2020.Week4
+{
+    public class Day23
+    {
+    }
+}
