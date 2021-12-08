77c4fad54ed27685031358e1f0e4dbbb39ad1932
Dominik Ruman <dr124@o2.pl>
2021-12-08 12:10:39 +0100

    Update Day8.cs

--- Advent._2021/Week2/Day8.cs
+++ Advent._2021/Week2/Day8.cs
@@ -4,7 +4,7 @@ namespace Advent._2021.Week2;
 
 internal class Day8 : IReadInputDay
 {
-    public (char[][] signals, char[][] output)[] Input { get; set; }
+    public (string[] signals, string[] output)[] Input { get; set; }
 
     public void ReadData()
     {
@@ -12,25 +12,33 @@ internal class Day8 : IReadInputDay
             .Select(x => x.Split(new[] { " ", "|" }, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries))
             .Select(x => (signals: x[..10], output: x[10..]))
             .Select(x=> (
-                x.signals.Select(y => y.OrderBy(c => c).ToArray()).OrderBy(y => y.Length).ToArray(), 
-                x.output.Select(y => y.OrderBy(c => c).ToArray()).ToArray()))
+                x.signals.Select(y => new string(y.OrderBy(c => c).ToArray())).OrderBy(y => y.Length).ToArray(), 
+                x.output.Select(y => new string(y.OrderBy(c => c).ToArray())).ToArray()))
             .ToArray();
     }
 
     public object TaskA() => Input.Sum(z => z.output.Count(y => y.Length is <= 4 or 7));
 
-    public object TaskB() => Input.Select(XD).Sum();
+    public object TaskB() => Input
+        .Select(x => XDD(x.signals, x.output))
+        .Select(x => int.Parse(string.Join("", x)))
+        .Sum();
 
-    int XD((char[][] signals, char[][] output) input)
+    int XDD(string[] signals, string[] output)
     {
-        var s = input.signals;
-        
-        var _1 = s[0];
-        var _7 = s[1];
-        var _4 = s[2];
-        var _8 = s[^1];
-        var _235 = s[3..6];
-        var _690 = s[6..9];
+        var parser = ParseSignals(signals);
+        var numbers = output.Select(parser);
+        return int.Parse(string.Join("", numbers));
+    }
+
+    Func<string, int> ParseSignals(string[] signals)
+    {
+        var _1 = signals[0];
+        var _7 = signals[1];
+        var _4 = signals[2];
+        var _8 = signals[^1];
+        var _235 = signals[3..6];
+        var _690 = signals[6..9];
         var _4_exc1 = _4.Except(_1).ToArray();
 
         var _5 = _235.First(x => x.Intersect(_4_exc1).Count() == _4_exc1.Length);
@@ -41,21 +49,15 @@ internal class Day8 : IReadInputDay
         var _6 = _690.First(x => x != _9 && x.Intersect(_5).Count() == _5.Length);
         var _0 = _690.First(x => x != _6 && x != _9);
 
-        var n = input.output.Select(x =>
+        return s =>
         {
-            if (x.SequenceEqual(_1)) return 1;
-            if (x.SequenceEqual(_2)) return 2;
-            if (x.SequenceEqual(_3)) return 3;
-            if (x.SequenceEqual(_4)) return 4;
-            if (x.SequenceEqual(_5)) return 5;
-            if (x.SequenceEqual(_6)) return 6;
-            if (x.SequenceEqual(_7)) return 7;
-            if (x.SequenceEqual(_8)) return 8;
-            if (x.SequenceEqual(_9)) return 9;
-            if (x.SequenceEqual(_0)) return 0;
+            if (s == _1) return 1; if (s == _2) return 2;
+            if (s == _3) return 3; if (s == _4) return 4;
+            if (s == _5) return 5; if (s == _6) return 6;
+            if (s == _7) return 7; if (s == _8) return 8;
+            if (s == _9) return 9; if (s == _0) return 0;
             return -1;
-        }).ToArray();
-
-        return int.Parse(string.Join("", n));
+        };
     }
+
 }
