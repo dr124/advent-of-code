b7f0572e90889323a261307b531400b830219a00
Dominik Ruman <dr124@o2.pl>
2021-12-19 18:30:49 +0100

    Update Day19.cs

--- Advent._2021/Week3/Day19.cs
+++ Advent._2021/Week3/Day19.cs
@@ -4,118 +4,54 @@ using Point = Vec3;
 
 internal class Day19 : IReadInputDay
 {
-    private List<Point> scannerPosition;
-    
-    class Map
-    {
-        public List<Point> _mapCopy;
-        public List<Point> _map;
-        public Point[,] Diffs;
-        public int rotation = 0;
-        public HashSet<Point> distances = new();
-        public Map(List<Point> list)
-        {
-            _mapCopy = list;
-            Restore();
-            CalculateDiffs();
-        }
-
-        public void RotateAll()
-        {
-            Restore();
-            rotation = ( rotation + 1 ) % 24;
-            for (int i = 0; i < _map.Count; i++)
-                _map[i] = Rotate(_map[i], rotation);
-            CalculateDiffs();
-        }
-
-        public void Restore()
-        {
-            _map = _mapCopy.Select(x => (Point)(x.X, x.Y, x.Z)).ToList();
-        }
-
-        public void CalculateDiffs()
-        {
-            Diffs = new Point[_map.Count, _map.Count];
-            distances.Clear();;
-            for (int i = 0; i < _map.Count; i++)
-            for (int j = 0; j <= i; j++)
-                if (i != j)
-                {
-                    Diffs[i, j] = (_map[i] - _map[j]);//.Abs();
-                    distances.Add(Diffs[i, j]);
-                }
-        }
-
-        public (Point p1, Point p2) GetPointFromDiff(Point diff)
-        {
-            for (int i = 0; i < _map.Count; i++)
-            for (int j = 0; j <= i; j++)
-                if (Diffs[i, j] == diff)
-                    return (_map[i], _map[j]);
-            return (null, null)!;
-        }
-
-        public void TranslateAll(Point vec)
-        {
-            for (int i = 0; i < _map.Count; i++)
-                _map[i] += vec;
-        }
-
-        public void MergeMap(Map m)
-        {
-            var h = _map.ToHashSet();
-            h.UnionWith(m._map);
-            _map = h.ToList();
-            CalculateDiffs();
-        }
-    }
-
-    private List<Map> Maps;
+    private readonly List<Point> ScannerPositions = new() { (0, 0, 0) };
+    private readonly List<Map> Maps = new ();
 
     public void ReadData()
     {
         var lines = File.ReadAllLines("Week3/Day19.txt");
-        List<Map> scanners = new();
         List<Point> list = new();
         foreach (var line in lines)
-            if (line.Contains("---")) // --- scanner X ---
+            if (line.Contains("---"))
                 list = new();
             else if (string.IsNullOrWhiteSpace(line))
-                scanners.Add(new Map(list));
+                Maps.Add(new Map(list));
             else
                 list.Add(Point.FromString(line));
-        scanners.Add(new Map(list));
-        Maps = scanners;
+        Maps.Add(new Map(list));
     }
 
     public object TaskA()
     {
-        var scanner1 = Maps[0];
-        var MapsLeft = Maps.Skip(1).ToList();
+        var mainMap = Maps[0];
+        var mapsLeft = Maps.Skip(1).ToList();
 
-
-        scannerPosition = new() { (0, 0, 0) };
-        for(int i = 0; i < MapsLeft.Count; i++)
+        for(int i = 0; mapsLeft.Count > 0; i++)
         {
-            var scanner2 = MapsLeft[i];
-            for (int n = 0; n< 50; n++)
+            var otherMap = mapsLeft[i];
+            for (int n = 0; n < 50; n++)
             {
-                scanner2.RotateAll();
-                var same = scanner2.distances.Where(x => scanner1.distances.Contains(x)).ToList();
+                otherMap.RotateAll();
+                var same = otherMap.Distances.Where(x => mainMap.Distances.Contains(x)).ToList();
                 if (same.Count >= 3)
                 {
-                    MergeMaps(scanner1, scanner2, same, out var distance);
-                    scannerPosition.Add(distance);
-                    MapsLeft.RemoveAt(i);
+                    MergeMaps(mainMap, otherMap, same, out var distance);
+                    ScannerPositions.Add(distance);
+                    mapsLeft.Remove(otherMap);
                     i = -1;
                     break;
                 }
             }
         }
-
-        return null;
+        return mainMap.Points.Count;
     }
+
+    public object TaskB() => (
+            from p1 in ScannerPositions
+            from p2 in ScannerPositions
+            where p1 != p2
+            select (p1 - p2).ManhattanDistance())
+        .Max();
 
     void MergeMaps(Map m1, Map m2, List<Point> distancesInBoth, out Point scannerPos)
     {
@@ -123,20 +59,10 @@ internal class Day19 : IReadInputDay
         var d1m1 = m1.GetPointFromDiff(d1);
         var d1m2 = m2.GetPointFromDiff(d1);
         var diff1 = d1m1.p1 - d1m2.p1;
-        var diff2 = d1m1.p2 - d1m2.p2;
         m2.TranslateAll(diff1);
-        d1m2 = m2.GetPointFromDiff(d1);
         m1.MergeMap(m2);
         scannerPos = diff1;
     }
-
-
-    public object TaskB() => (
-            from p1 in scannerPosition
-            from p2 in scannerPosition
-            where p1 != p2
-            select (p1 - p2).ManhattanDistance())
-        .Max();
 
     static Point Rotate(Point v, int n)
     {
@@ -168,5 +94,70 @@ internal class Day19 : IReadInputDay
             22 => (-z, y, x),
             23 => (-z, -y, -x),
         };
+    }
+
+    class Map
+    {
+        private List<Point> _mapCopy;
+        public List<Point> Points;
+        public Point[,] Diffs;
+        public int Rotation = 0;
+        public HashSet<Point> Distances = new();
+        public Map(List<Point> list)
+        {
+            _mapCopy = list;
+            Restore();
+            CalculateDiffs();
+        }
+
+        public void RotateAll()
+        {
+            Restore();
+            Rotation = (Rotation + 1) % 24;
+            for (int i = 0; i < Points.Count; i++)
+                Points[i] = Rotate(Points[i], Rotation);
+            CalculateDiffs();
+        }
+
+        public void Restore()
+        {
+            Points = _mapCopy.Select(x => (Point)(x.X, x.Y, x.Z)).ToList();
+        }
+
+        public void CalculateDiffs()
+        {
+            Diffs = new Point[Points.Count, Points.Count];
+            Distances.Clear();
+            for (int i = 0; i < Points.Count; i++)
+            for (int j = 0; j <= i; j++)
+                if (i != j)
+                {
+                    Diffs[i, j] = Points[i] - Points[j];
+                    Distances.Add(Diffs[i, j]);
+                }
+        }
+
+        public (Point p1, Point p2) GetPointFromDiff(Point diff)
+        {
+            for (int i = 0; i < Points.Count; i++)
+            for (int j = 0; j <= i; j++)
+                if (Diffs[i, j] == diff)
+                    return (Points[i], Points[j]);
+            throw new ArgumentOutOfRangeException(nameof(diff));
+        }
+
+        public void TranslateAll(Point vec)
+        {
+            for (int i = 0; i < Points.Count; i++)
+                Points[i] += vec;
+        }
+
+        public void MergeMap(Map otherMap)
+        {
+            var h = Points.ToHashSet();
+            h.UnionWith(otherMap.Points);
+            Points = h.ToList();
+            CalculateDiffs();
+        }
     }
 }
