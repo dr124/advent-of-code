6c2800aeea36d9feb0492a148d50ab5ef4bc5ae9
Dominik Ruman <dr124@o2.pl>
2021-12-21 22:26:02 +0100

    Update Day21.cs

--- Advent._2021/Week3/Day21.cs
+++ Advent._2021/Week3/Day21.cs
@@ -1,26 +1,34 @@
-﻿using System.Text;
+﻿using System.Runtime.CompilerServices;
 using Advent.Core;
-using Advent.Core.Extensions;
-using BenchmarkDotNet.Attributes;
 
 namespace Advent._2021.Week3;
 
 internal class Day21 : IReadInputDay
 {
+    private readonly Dictionary<State, Result> _dict = new();
+    private readonly (int value, int number)[] AllRolls = (
+            from i in Enumerable.Range(1, 3)
+            from j in Enumerable.Range(1, 3)
+            from k in Enumerable.Range(1, 3)
+            let roll = i + j + k
+            select roll)
+        .GroupBy(x => x)
+        .Select(x => (x.Key, x.Count()))
+        .ToArray();
+
     public void ReadData()
     {
-      
     }
+    private static int Mod10(int a) => (a - 1) % 10 + 1;
 
-    int Mod10(int a) => (a - 1) % 10 + 1;
 
     public object TaskA()
     {
-        int score1 = 0;
-        int score2 = 0;
-        int pos1 = 7;
-        int pos2 = 2;
-        int rolled = 0;
+        var score1 = 0;
+        var score2 = 0;
+        var pos1 = 7;
+        var pos2 = 2;
+        var rolled = 0;
         while (true)
         {
             //p1
@@ -36,62 +44,35 @@ internal class Day21 : IReadInputDay
             pos2 = Mod10(pos2 + r2);
             score2 += pos2;
 
-            if (score2 >= 1000) 
+            if (score2 >= 1000)
                 return score1 * rolled;
         }
-
     }
 
+    public object TaskB() => Play(new State(true, 7, 2, 0, 0)).Max();
 
-    int _pos1 = 7;
-    int _pos2 = 2;
-
-    private (int v,int n)[] rolls;
-    public object TaskB()
+    private Result Play(State state) => state switch
     {
-        rolls = (
-                from i in Enumerable.Range(1, 3)
-                from j in Enumerable.Range(1, 3)
-                from k in Enumerable.Range(1, 3)
-                let roll = i + j + k
-                select roll)
+        { Score1: >= 21 } => new(1, 0),
+        { Score2: >= 21 } => new(0, 1),
+        _ => AllRolls.Aggregate(Result.Zero, (result, roll) => result + PlayCached(NextState(state, roll.value)) * roll.number)
+    };
 
-            .GroupBy(x=>x)
-            .Select(x=>(x.Key, x.Count()))
-            .ToArray();
-        var r = XD(true, 7, 2, 0, 0);
-        return r.Item1 > r.Item2 ? r.Item1 : r.Item2;
-    }
-    
-
-    public (long, long) XD(bool isP1, int pos1, int pos2, int score1, int score2)
+    private State NextState(State s, int roll) => s switch
     {
-        if (score1 >= 21)
-            return (1, 0);
-        if (score2 >= 21) 
-            return (0, 1);
+        { IsPlayer1: true } => new(!s.IsPlayer1, Mod10(s.Pos1 + roll), s.Pos2, s.Score1 + Mod10(s.Pos1 + roll), s.Score2),
+        { IsPlayer1: false } => new(!s.IsPlayer1, s.Pos1, Mod10(s.Pos2 + roll), s.Score1, s.Score2 + Mod10(s.Pos2 + roll))
+    };
 
-        if (isP1)
-        {
-            var x = (0L, 0L);
-            foreach (var roll in rolls)
-            {
-                 var a = XD(!isP1, Mod10(pos1 + roll.v), pos2, score1 + Mod10(pos1 + roll.v), score2);
-                 x.Item1 += roll.n * a.Item1;
-                 x.Item2 += roll.n * a.Item2;
-            }
-            return x;
-        }
-        else
-        {
-            var x = (0L, 0L);
-            foreach (var roll in rolls)
-            {
-                var a = XD(!isP1, pos1, Mod10(pos2 + roll.v), score1, score2 + Mod10(pos2 + roll.v));
-                x.Item1 += roll.n * a.Item1;
-                x.Item2 += roll.n * a.Item2;
-            }
-            return x;
-        }
+
+    private Result PlayCached(State s) => _dict.TryGetValue(s, out var res) ? res : _dict[s] = Play(s);
+
+    private readonly record struct Result(long A, long B)
+    {
+        public static Result Zero => new(0, 0);
+        public long Max() => A > B ? A : B;
+        public static Result operator +(Result a, Result b) => new(a.A + b.A, a.B + b.B);
+        public static Result operator *(Result a, int b) => new(a.A * b, a.B * b);
     }
+    private record struct State(bool IsPlayer1, int Pos1, int Pos2, int Score1, int Score2);
 }
