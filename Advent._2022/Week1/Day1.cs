6b7d24c97e5ce31214409546e3b1a93f08d2c52e
Dominik Ruman <dr124@users.noreply.github.com>
2022-12-01 15:14:21 +0100

    Day1 ⭐⭐

--- Advent._2022/Week1/Day1.cs
+++ Advent._2022/Week1/Day1.cs
@@ -4,18 +4,20 @@ namespace Advent._2022.Week1;
 
 public class Day1 : IReadInputDay
 {
+    private List<int> _input;
+
     public void ReadData()
     {
-        File.ReadAllLines("Week1/Day1.txt");
-    }
-    
-    public object? TaskA()
-    {
-        throw new NotImplementedException();
+        _input = File.ReadLines("Week1/Day1.txt")
+            .Aggregate(new List<int> { 0 }, (list, line) =>
+            {
+                if (line is "") list.Add(0);
+                else list[^1] += int.Parse(line);
+                return list;
+            });
     }
 
-    public object? TaskB()
-    {
-        throw new NotImplementedException();
-    }
+    public object? TaskA() => _input.Max();
+
+    public object? TaskB() => _input.OrderDescending().Take(3).Sum();
 }
