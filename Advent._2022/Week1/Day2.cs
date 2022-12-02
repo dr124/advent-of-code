8b05c10724ef86726ca993c92c5b18e9f0d4a832
Dominik Ruman <dr124@users.noreply.github.com>
2022-12-02 11:40:08 +0100

    Day2 ⭐⭐

--- Advent._2022/Week1/Day2.cs
+++ Advent._2022/Week1/Day2.cs
@@ -4,18 +4,70 @@ namespace Advent._2022.Week1;
 
 public class Day2 : IReadInputDay
 {
+    private List<(Shape opponent, Shape me)> _input;
     public void ReadData()
     {
-        File.ReadAllLines("Week1/Day2.txt");
+        _input = File.ReadAllLines("Week1/Day2.txt")
+            .Select(x => ((Shape)(x[0]-'A'), (Shape)(x[2]-'X')))
+            .ToList();
+    }
+    
+    public object? TaskA() => _input
+            .Select(x => RoundResult(x.opponent, x.me) + ShapeScore(x.me))
+            .Sum();
+
+    public object? TaskB() => _input
+            .Select(x => (x.opponent, me: FromRoundResult(x.opponent, (Result)x.me)))
+            .Select(x => RoundResult(x.opponent, x.me) + ShapeScore(x.me))
+            .Sum();
+    
+    
+    private static int RoundResult(Shape opponent, Shape me)
+    {
+        return (opponent, me) switch
+            {
+                (Shape.Rock, Shape.Paper) => Result.Win,
+                (Shape.Rock, Shape.Scissors) => Result.Loss,
+                (Shape.Paper, Shape.Rock) => Result.Loss,
+                (Shape.Paper, Shape.Scissors) => Result.Win,
+                (Shape.Scissors, Shape.Rock) => Result.Win,
+                (Shape.Scissors, Shape.Paper) => Result.Loss,
+                (_, _) x when x.opponent == x.me => Result.Draw,
+            } switch
+            {
+                Result.Win => 6,
+                Result.Draw => 3,
+                Result.Loss => 0,
+            };
+    }
+    
+    private static Shape FromRoundResult(Shape opponent, Result result)
+    {
+        return (opponent, result) switch
+        {
+            (Shape.Rock, Result.Win) => Shape.Paper,
+            (Shape.Rock, Result.Loss) => Shape.Scissors,
+            (Shape.Paper, Result.Loss) => Shape.Rock,
+            (Shape.Paper, Result.Win) => Shape.Scissors,
+            (Shape.Scissors, Result.Win) => Shape.Rock,
+            (Shape.Scissors, Result.Loss) => Shape.Paper,
+            (_, Result.Draw) x => opponent
+        };
     }
 
-    public object? TaskA()
+    private static int ShapeScore(Shape me) => (int)me + 1;
+
+    private enum Shape
     {
-        throw new NotImplementedException();
+        Rock = 0,
+        Paper = 1,
+        Scissors = 2,
     }
 
-    public object? TaskB()
+    private enum Result
     {
-        throw new NotImplementedException();
+        Loss = 0,
+        Draw = 1,
+        Win = 2
     }
 }
