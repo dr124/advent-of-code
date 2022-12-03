95a78f903facc110d5fa2978013485fe94faa778
Dominik Ruman <dr124@users.noreply.github.com>
2022-12-03 09:35:16 +0100

    Day3 ⭐⭐

--- Advent._2022/Week1/Day3.cs
+++ Advent._2022/Week1/Day3.cs
@@ -4,18 +4,33 @@ namespace Advent._2022.Week1;
 
 public class Day3 : IReadInputDay
 {
-    public void ReadData()
-    {
-        File.ReadAllLines("Week1/Day3.txt");
-    }
+    private string[] _input;
+    
+    public void ReadData() => _input 
+        = File.ReadAllLines("Week1/Day3.txt");
 
-    public object? TaskA()
-    {
-        throw new NotImplementedException();
-    }
+    public object TaskA() => _input
+        .Select(x => new[] { x[..(x.Length / 2)], x[(x.Length / 2)..] })
+        .Select(CommonCharacter)
+        .Select(Priority)
+        .Sum();
 
-    public object? TaskB()
+    public object TaskB() => _input
+        .Chunk(3)
+        .Select(CommonCharacter)
+        .Select(Priority)
+        .Sum();
+    
+    private static char CommonCharacter(params string[] str) => str
+        .SelectMany(x => x.Distinct())
+        .GroupBy(x => x)
+        .MaxBy(x => x.Count())!
+        .Key;
+
+    private static int Priority(char c) => c switch
     {
-        throw new NotImplementedException();
-    }
+        >= 'a' and <= 'z' => c - 'a' + 1,
+        >= 'A' and <= 'Z' => c - 'A' + 27,
+        _ => throw new Exception("what")
+    };
 }
