6297ccfe668fcb392548582b96f042be06e969fc
Dominik Ruman <dr124@users.noreply.github.com>
2022-12-04 11:29:27 +0100

    Day4 ⭐⭐

--- Advent._2022/Week1/Day4.cs
+++ Advent._2022/Week1/Day4.cs
@@ -4,18 +4,29 @@ namespace Advent._2022.Week1;
 
 public class Day4 : IReadInputDay
 {
-    public void ReadData()
-    {
-        File.ReadAllLines("Week1/Day4.txt");
-    }
+    private Pair[] _input;
 
-    public object? TaskA()
-    {
-        throw new NotImplementedException();
-    }
+    public void ReadData() => _input =
+        File.ReadLines("Week1/Day4.txt")
+            .Select(line => line
+                .Split(',', '-')
+                .Select(int.Parse)
+                .ToArray())
+            .Select(x => new Pair(new(x[0], x[1]), new(x[2], x[3])))
+            .ToArray();
 
-    public object? TaskB()
-    {
-        throw new NotImplementedException();
-    }
+    public object? TaskA() => _input.Count(IsFullyContained);
+
+    public object? TaskB() => _input.Count(IsPartiallyContained);
+
+    private bool IsFullyContained(Pair pair) =>
+        (pair.A.Start >= pair.B.Start && pair.A.End <= pair.B.End)
+        || (pair.B.Start >= pair.A.Start && pair.B.End <= pair.A.End);
+
+    private bool IsPartiallyContained(Pair pair) =>
+        pair.A.Start <= pair.B.End && pair.A.End >= pair.B.Start;
+    
+    private record Section(int Start, int End);
+
+    private record Pair(Section A, Section B);
 }
