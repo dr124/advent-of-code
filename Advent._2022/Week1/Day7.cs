a405bcc3cfabeaa9aac8b15e2b4d27cf8ec3ed0d
Dominik Ruman <dr124@users.noreply.github.com>
2022-12-07 14:57:21 +0100

    Day7 ⭐⭐

--- Advent._2022/Week1/Day7.cs
+++ Advent._2022/Week1/Day7.cs
@@ -4,18 +4,73 @@ namespace Advent._2022.Week1;
 
 public class Day7 : IReadInputDay
 {
+    private static List<Node> _allNodes = new();
+    private static Node _root = new(null, "/", null);
+    
     public void ReadData()
     {
-        File.ReadAllLines("Week1/Day7.txt");
+        void InsertNode(Node parent, Node newNode)
+        {
+            parent.Children.Add(newNode);
+            _allNodes.Add(newNode);
+        }
+
+        var pointer = _root;
+        foreach (var line in File.ReadLines("Week1/Day7.txt"))
+        { 
+            var arg = line.Split(' ');
+            if (arg[0] is "$")
+            {
+                if (arg[1] is "cd")
+                {
+                    pointer = pointer[arg[2]];
+                }
+            }
+            else if (arg[0] is "dir")
+            {
+                InsertNode(pointer, new Node(pointer, arg[1], null));
+            }
+            else
+            {
+                InsertNode(pointer, new Node(pointer, arg[1], long.Parse(arg[0])));
+            }
+        }
     }
 
     public object? TaskA()
     {
-        throw new NotImplementedException();
+        return _allNodes
+            .Where(x => x.Size is null)
+            .Select(x => x.TotalSize())
+            .Where(x => x <= 100000)
+            .Sum();
     }
 
     public object? TaskB()
     {
-        throw new NotImplementedException();
+        var totalSum = _root.TotalSize();
+        var disk = 70000000;
+        var needed = 30000000;
+        var remain = disk - totalSum;
+        var minSizeToDelete = needed - remain;
+
+        return _allNodes
+            .Where(x => x.Size is null)
+            .Select(x => x.TotalSize())
+            .Where(x => x >= minSizeToDelete)
+            .Min();
+    }
+
+    public record Node(Node Parent, string Name, long? Size) 
+    {
+        public List<Node> Children { get; } = new();
+        public long TotalSize() => Size ?? Children.Sum(c => c.TotalSize());
+        public Node this[string name] => name switch
+        {
+            ".." => Parent,
+            "/" => _root,
+            "." => this,
+            _ => Children.First(c => c.Name == name)
+        };
     }
 }
