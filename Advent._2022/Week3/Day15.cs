f8a83c15e1f38fa5f3fb3cd339cf452adf46fc39
Dominik Ruman <dr124@users.noreply.github.com>
2022-12-15 12:10:02 +0100

    x

--- Advent._2022/Week3/Day15.cs
+++ Advent._2022/Week3/Day15.cs
@@ -21,33 +21,54 @@ public class Day15 : IReadInputDay
         }
     }
 
+    IEnumerable<Vec2> Spiral( int X, int Y){
+        int x,y,dx,dy;
+        x = y = dx =0;
+        dy = -1;
+        int t = Math.Max(X,Y);
+        int maxI = t*t;
+        for(int i =0; i < maxI; i++){
+            if ((-X/2 <= x) && (x <= X/2) && (-Y/2 <= y) && (y <= Y/2))
+            {
+                yield return (x, y);
+            }
+            if( (x == y) || ((x < 0) && (x == -y)) || ((x > 0) && (x == 1-y))){
+                t = dx;
+                dx = -dy;
+                dy = t;
+            }
+            x += dx;
+            y += dy;
+        }
+    }
+    
     public object? TaskA()
     {
-        int sum = 0;
-        int y = 10;
-        for (int x = 0;; x++)
-        {
-            if (_beacons.Contains((x,y))) 
-                continue;
+        var centerX = (int)_beacons.Select(x => x.X).Average();
+        var centerY = (int)_beacons.Select(x => x.Y).Average();
 
-            if (!IsInRange((x, y)))
-                break;
-            
-            sum++;
-        }
-        
-        for (int x = -1;; x--)
+        for (int r = 1;r < 2_000_000; r++)
         {
-            if (_beacons.Contains((x,y))) 
-                continue;
+            for (int x = -r; x <= r; x++)
+            {
+                for (int y = x - r; y <= r - x; y++)
+                {
+                    if(Math.Abs(x) + Math.Abs(y) != r)
+                        continue;
 
-            if (!IsInRange((x, y)))
-                break;
-            
-            sum++;
+                    Vec2 p = (x + centerX, y + centerY);
+                    var res = IsNotInRange(p);
+                    if (res)
+                    {
+                        return p.X * 4_000_000 + p.Y;
+                    }
+                }
+                
+            }
+            Console.WriteLine($"r : {r}");
         }
-        
-        return sum;
+
+        return -1;
     }
     
     bool IsInRange(Vec2 p)
@@ -65,6 +86,23 @@ public class Day15 : IReadInputDay
         }
 
         return false;
+    }
+    
+    bool IsNotInRange(Vec2 p)
+    {
+        for(int i = 0; i < _sensors.Count; i++)
+        {
+            var s = _sensors[i];
+            var b = _beacons[i];
+            var l = ManhattanDistance(s, b);
+            var lps = ManhattanDistance(p, s);
+            if (lps <= l)
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     private int ManhattanDistance(Vec2 v1, Vec2 v2)
