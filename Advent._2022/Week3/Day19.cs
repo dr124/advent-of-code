8a1449642999e9d796613a9ee4596d1131357bf5
Dominik Ruman <dr124@users.noreply.github.com>
2022-12-20 01:15:37 +0100

    x

--- Advent._2022/Week3/Day19.cs
+++ Advent._2022/Week3/Day19.cs
@@ -1,7 +1,4 @@
-﻿using Advent.Core;
-using Advent.Core.Extensions;
-using Microsoft.Diagnostics.Tracing.Parsers.AspNet;
-using Random = System.Random;
+﻿using Advent.Core.Extensions;
 
 namespace Advent._2022.Week3;
 
@@ -9,127 +6,84 @@ public class Day19 : IReadInputDay
 {
     private Blueprint[] _input;
 
-    private record Blueprint(int Id, int OreRobotOreCost, int ClayRobotOreCost, int ObsidianRobotOreCost, int ObsidianRobotClayCost, int GeodeRobotOreCost, int GeodeRobotObsidianCost)
-    {
-
-        public static Blueprint Create(string line)
-        {
-            var parts = line.ExtractNumbers();
-            return new Blueprint(parts[0], parts[1], parts[2], parts[3], parts[4], parts[5], parts[6]);
-        }
-    }
-
     public void ReadData()
     {
         _input = File.ReadLines("Week3/Day19.txt")
             .Select(Blueprint.Create)
             .ToArray();
-
     }
 
-    public object? TaskA()
-    {
-        var xd = _input.Take(3).Select(CalculateMaximumProduction).Product();
+    public object? TaskA() => null;//_input.Take(3).Select(CalculateMaximumProduction).Product();
 
-        return xd;
-    }
+    public object? TaskB() => _input.Take(3).Select(CalculateMaximumProduction).Product();
 
     private int CalculateMaximumProduction(Blueprint blueprint)
     {
-        var max = Enumerable.Range(0, 1000000).AsParallel()
-            .Select(i => Production(new Random(i), blueprint)).Max();
+        var max = Enumerable.Range(0, 2000000).AsParallel()
+            .Select(i => Production(new Random(i), blueprint, 32)).Max();
         Console.WriteLine(max);
         return max;
     }
 
-    const int max = 32;
 
-    private int Production(Random rnd, Blueprint bp)
+    private int Production(Random rnd, Blueprint bp, int max)
     {
         var minute = 0;
         int oreR = 1, clayR = 0, obsR = 0, geoR = 0;
         int ore = 0, clay = 0, obs = 0, geo = 0;
-        
+
         while (true)
         {
-            if (minute >= max)
-            {
-                return geo;
-            }
+            if (minute >= max) return geo;
+            var wait = rnd.NextSingle() < 0.1f;
+            var skipGeoRobot = rnd.NextSingle() < 0.1f;
+            var skipObsRobot = rnd.NextSingle() < 0.1f;
+            var skipClayRobot = rnd.NextSingle() < 0.1f;
 
-            var wait = rnd.NextSingle() < 0.2f;
-            var skipRobot1 = rnd.NextSingle() < 0.2f;
-            var skipRobot2 = rnd.NextSingle() < 0.2f;
-            var skipRobot3 = rnd.NextSingle() < 0.2f;
+            ore += oreR;
+            clay += clayR;
+            obs += obsR;
+            geo += geoR;
 
             // if can make geo robot
-            if (!wait && obs >= bp.GeodeRobotObsidianCost && ore >= bp.GeodeRobotOreCost && !skipRobot1)
+            if (!wait && !skipGeoRobot && obs - obsR >= bp.GeodeRobotObsidianCost && ore - oreR >= bp.GeodeRobotOreCost)
             {
                 obs -= bp.GeodeRobotObsidianCost;
                 ore -= bp.GeodeRobotOreCost;
-                ore += oreR;
-                clay += clayR;
-                obs += obsR;
-                geo += geoR;
-                
                 geoR += 1;
             }
             //if can make obs robot
-            else if (!wait && clay >= bp.ObsidianRobotClayCost && ore >= bp.ObsidianRobotOreCost && !skipRobot2)
+            else if (!wait && !skipObsRobot && clay - clayR >= bp.ObsidianRobotClayCost &&
+                     ore - oreR >= bp.ObsidianRobotOreCost)
             {
                 clay -= bp.ObsidianRobotClayCost;
                 ore -= bp.ObsidianRobotOreCost;
-                ore += oreR;
-                clay += clayR;
-                obs += obsR;
-                geo += geoR;
-                
                 obsR += 1;
             }
             // if can make clay robot
-            else if (!wait && ore >= bp.ClayRobotOreCost && !skipRobot3)
+            else if (!wait && !skipClayRobot && ore - oreR >= bp.ClayRobotOreCost)
             {
                 ore -= bp.ClayRobotOreCost;
-                ore += oreR;
-                clay += clayR;
-                obs += obsR;
-                geo += geoR;
                 clayR += 1;
             }
             // if can make ore robot
-            else if (!wait && ore >= bp.OreRobotOreCost)
+            else if (!wait && ore - oreR >= bp.OreRobotOreCost)
             {
                 ore -= bp.OreRobotOreCost;
-                ore += oreR;
-                clay += clayR;
-                obs += obsR;
-                geo += geoR;
                 oreR += 1;
-            }
-            else
-            {
-                ore += oreR;
-                clay += clayR;
-                obs += obsR;
-                geo += geoR;
             }
 
             minute += 1;
         }
     }
 
-    public object? TaskB()
+    private record Blueprint(int Id, int OreRobotOreCost, int ClayRobotOreCost, int ObsidianRobotOreCost,
+        int ObsidianRobotClayCost, int GeodeRobotOreCost, int GeodeRobotObsidianCost)
     {
-        return null;
-    }
-
-    private record Robot(Mineral Type);
-
-    private enum Mineral
-    {
-        Ore,
-        Clay,
-        Obsidian,
-        Geode
+        public static Blueprint Create(string line)
+        {
+            var parts = line.ExtractNumbers();
+            return new Blueprint(parts[0], parts[1], parts[2], parts[3], parts[4], parts[5], parts[6]);
+        }
     }
 }
